dc270222c7f4e831815941cd86af0a0ddc8e531b
ebertinbench <agenthunk27@gmail.com>
2025-12-09 09:23:41 +0100

    commiting new gh pages variable

--- ethan-portfolio/vite.config.js
+++ ethan-portfolio/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+  base: '/portfolio/',
   plugins: [
     vue(),
     vueDevTools(),
